a6049456157ef736677bc1f8e6090877cf74b023
Sergei Litovchenko <serglit72@gmail.com>
2023-09-06 21:52:50 -0700

    Initial module

---
+++ modules/variables.tf
@@ -0,0 +1,30 @@
+variable "token" {
+  type        = string
+  description = "github token to connect github repo"
+  default     = "Your Gitub Token"
+}
+
+variable "repository" {
+  type        = string
+  description = "github repo url"
+  default     = "https://github.com/serglit72/amplify-app.git"
+}
+
+variable "app_name" {
+  type        = string
+  description = "AWS Amplify App Name"
+  default     = "my-amplify-app"
+}
+
+variable "branch_name" {
+  type        = string
+  description = "AWS Amplify App Repo Branch Name"
+  default     = "main"
+}
+
+
+variable "domain_name" {
+  type        = string
+  default     = "awsamplifyapp.com"
+  description = "AWS Amplify Domain Name"
+}
